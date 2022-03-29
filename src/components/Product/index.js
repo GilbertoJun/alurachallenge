f9d906ae310223de9b35747238155bd68a06391f
GilbertoJun <15beto15@gmail.com>
2022-03-29 17:49:36 -0400

    Iniciando adição de produtos

---
+++ src/components/Product/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+
+function Product(){
+    return <div>
+        <img src="" />
+        <div className="label">
+            <label>Produto</label>
+            <span>R$ 60,00</span>
+            <a>Ver produto</a>
+        </div>
+
+    </div>
+}
+
+export default Product;
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -2,11 +2,18 @@ import React from 'react';
 import ReactDOM from "react-dom";
 
 import Top from '../../components/Top'
+import Product from '../../components/Product';
 
 function Home(){
 
     return <React.Fragment>
         <Top />
+        <div className="slider">
+        </div>
+        <div className="container-principal container-produtos">
+            <Product />
+        </div>
+
     </React.Fragment>;
 }
 
